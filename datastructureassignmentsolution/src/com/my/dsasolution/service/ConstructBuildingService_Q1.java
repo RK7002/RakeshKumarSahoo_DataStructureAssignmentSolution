8936de6c6bd876031287996df223b26b85f97316
RK7002 <98643689+RK7002@users.noreply.github.com>
2022-05-15 06:57:07 +0530

    Update ConstructBuildingService_Q1.java
    Added The Driver and Print Floor Details

--- datastructureassignmentsolution/src/com/my/dsasolution/service/ConstructBuildingService_Q1.java
+++ datastructureassignmentsolution/src/com/my/dsasolution/service/ConstructBuildingService_Q1.java
@@ -1,13 +1,32 @@
 package com.my.dsasolution.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import com.my.dsasolution.driver.ConstructBuildingDriver;
 
 public class ConstructBuildingService_Q1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		List<Integer> floorSizeList = new ArrayList<Integer>();
+		
 		ConstructBuildingDriver obj = new ConstructBuildingDriver();
-		obj.message();
+		
+		// Taking Inputs
+		System.out.println("Enter the total no of floors in the building");
+		int noOfFloors = scanner.nextInt();
+		
+		for (int i = 0; i < noOfFloors; i++) {
+			System.out.println("Enter the Floor Size on Day " + (i + 1));
+			floorSizeList.add(scanner.nextInt());
+		}
+		
+		//Calling the driver method to print the requirement the assemble floor detail
+		System.out.println("\nThe order of construction is as follows");
+		obj.printOrderOfConstruction(floorSizeList, floorSizeList.size()-1);
+		
+		scanner.close();
 	}
-
 }
